42268330d69f30b06d78ace5d61f5454e8604730
Pedro Cardoso <phlc.cardoso@gmail.com>
2022-05-06 01:49:57 -0300

    adição do trigger no ddl

--- src/ddl/ddl_clinica_laboratorial.sql
+++ src/ddl/ddl_clinica_laboratorial.sql
@@ -111,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `TB_DEBITO` (
   `id_registro` INT NOT NULL,
   `valor` DECIMAL(10,2) NOT NULL,
   `dthora_registro` DATETIME NOT NULL,
-  `dthora_pagamento` DATETIME NOT NULL,
+  `dthora_pagamento` DATETIME,
   PRIMARY KEY (`id_debito`));
 
 -- -----------------------------------------------------------------------------------------------------------------
@@ -357,4 +357,43 @@ BEGIN
 	(16,5,2,null,STR_TO_DATE('16/10/2021 18:00','%d/%m/%Y %H:%i'));
 END//
 
+DELIMITER ;
+
+-- -----------------------------------------------------------------------------------------------------------------
+-- ----------------------------------------- CRIAÇÃO DOS TRIGGERS --------------------------------------------------
+-- -----------------------------------------------------------------------------------------------------------------
+
+-- tg_ai_insert_debito
+DROP TRIGGER IF EXISTS `tg_ai_insert_debito`;
+
+DELIMITER //
+
+CREATE TRIGGER `tg_ai_insert_debito`
+    AFTER INSERT
+    ON `TB_REGISTRO_EXAME` FOR EACH ROW
+		BEGIN
+			DECLARE valor DECIMAL(10,2);
+            DECLARE coparticipacao DECIMAL (4,3);
+			
+            SELECT ex.valor FROM TB_EXAME AS ex WHERE ex.id_exame = NEW.id_exame INTO valor;
+            SELECT cv.taxa_coparticipacao FROM TB_CONVENIO AS cv WHERE cv.id_convenio = NEW.id_convenio INTO coparticipacao;
+			
+			IF NEW.id_convenio IS NULL THEN
+				INSERT INTO `TB_DEBITO`
+                (`id_tipo`, `id_registro`, `valor`, `dthora_registro`, `dthora_pagamento`)
+                VALUES
+                (1,NEW.id_registro,valor, NOW(), null);
+			ELSE
+				INSERT INTO `TB_DEBITO`
+                (`id_tipo`, `id_registro`, `valor`, `dthora_registro`, `dthora_pagamento`)
+                VALUES
+                (1,NEW.id_registro,(valor * coparticipacao), NOW(), null);
+                
+                INSERT INTO `TB_DEBITO`
+                (`id_tipo`, `id_registro`, `valor`, `dthora_registro`, `dthora_pagamento`)
+                VALUES
+                (2,NEW.id_registro,(valor * (1-coparticipacao)), NOW(), null);
+            END IF;
+        END//        
+        
 DELIMITER ;
